13e2f7c6eb6ac02bd28c258180cadca3e1e94708
Chriszzz <chrisz2zz@duck.com>
2024-04-21 22:09:06 +0800

    finish ch1

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,13 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 
-use core::fmt::{self, Write};
-
+use log::{debug, error, info, trace, warn};
 use sbi::shutdown;
+mod console;
 mod lang_item;
+mod logging;
 mod sbi;
-
-const SYSCALL_EXIT: usize = 93;
-
-fn syscall(id: usize, args: [usize; 3]) -> isize {
-    let mut ret;
-    unsafe {
-        core::arch::asm!(
-            "ecall",
-            inlateout("x10") args[0] => ret,
-            in("x11") args[1],
-            in("x12") args[2],
-            in("x17") id,
-        );
-    }
-    ret
-}
-
-pub fn sys_exit(xstate: i32) -> isize {
-    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
-}
-
-const SYSCALL_WRITE: usize = 64;
-
-pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
-    syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
-}
-
-struct Stdout;
-
-impl Write for Stdout {
-    fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        sys_write(1, s.as_bytes());
-        Ok(())
-    }
-}
-
-pub fn print(args: fmt::Arguments) {
-    Stdout.write_fmt(args).unwrap()
-}
-
-#[macro_export]
-macro_rules! print {
-    ($fmt: literal $(, $($arg: tt)+)?) => {
-        $crate::console::print(format_args!($fmt $(,$($arg)+)?));
-    };
-}
-
-#[macro_export]
-macro_rules! println {
-    ($fmt: literal $(,$($arg:tt)+)?) => {
-        print(format_args!(concat!($fmt, "\n") $(,$($arg)+)?));
-    };
-}
 
 core::arch::global_asm!(include_str!("entry.asm"));
 
@@ -72,6 +21,44 @@ fn clear_bss() {
 
 #[no_mangle]
 pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext(); // begin addr of text segment
+        fn etext(); // end addr of text segment
+        fn srodata(); // start addr of Read-Only data segment
+        fn erodata(); // end addr of Read-Only data ssegment
+        fn sdata(); // start addr of data segment
+        fn edata(); // end addr of data segment
+        fn sbss(); // start addr of BSS segment
+        fn ebss(); // end addr of BSS segment
+        fn boot_stack_lower_bound(); // stack lower bound
+        fn boot_stack_top(); // stack top
+    }
+
     clear_bss();
-    shutdown();
+    logging::init();
+    println!("[kernel] Hello, world!");
+    trace!(
+        "[kernel .text [{:#x}, {:#x})",
+        stext as usize,
+        etext as usize
+    );
+
+    debug!(
+        "[kernel .rodata [{:#x}, {:#x})",
+        srodata as usize, erodata as usize
+    );
+
+    info!(
+        "[kernel .data [{:#x}, {:#x})",
+        sdata as usize, edata as usize
+    );
+
+    warn!(
+        "[kernel .boot_stack top=bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize, boot_stack_lower_bound as usize
+    );
+
+    error!("[kernel .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+
+    shutdown(false);
 }
--- src/sbi.rs
+++ src/sbi.rs
@@ -1,20 +1,22 @@
-fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
-    let mut ret;
-    unsafe {
-        core::arch::asm!(
-            "ecall",
-            inlateout("x10") arg0 => ret,
-            in("x11") arg1,
-            in("x12") arg2,
-            in("x17") which,
-        );
-    }
-    ret
+#![allow(unused)]
+
+pub fn console_putchar(c: usize) {
+    #[allow(deprecated)]
+    sbi_rt::legacy::console_putchar(c);
 }
 
-const SBI_SHUTDOWN: usize = 8;
+pub fn console_getchar() -> usize {
+    #[allow(deprecated)]
+    sbi_rt::legacy::console_getchar()
+}
 
-pub fn shutdown() -> ! {
-    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
-    panic!("It should shutdown!");
+pub fn shutdown(failture: bool) -> ! {
+    use sbi_rt::{system_reset, NoReason, Shutdown, SystemFailure};
+    if !failture {
+        system_reset(Shutdown, NoReason);
+    } else {
+        system_reset(Shutdown, SystemFailure);
+    }
+
+    unreachable!();
 }
